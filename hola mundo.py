4a53b44aab768eb41fc818fc86b3a3846d912d63
Alberto Guirado Méndez <al_aguiradom@iessierradelasvillas.org>
2022-09-29 10:03:11 +0200

    Comentarios, entrada, salida de datos y tipos de datos en python

---
+++ hola mundo.py
@@ -0,0 +1,18 @@
+#Entrada y salida de datos
+#Salida con print()
+
+
+
+
+nombre="Alberto"
+edad= 17
+mayorOmenorEdad= False
+
+
+print("buenos dias", nombre , "tu edad es" , edad)
+
+#Entrada de datos -- input()
+nombre= input("Dime tu nombre:\n") #\n Permite hacer un salto de linea
+edad= input("Dime tu edada:\n ")
+
+print("buenos dias", nombre , "tu edad es" , edad)
